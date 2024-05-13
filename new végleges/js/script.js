776757cdf0f04248ce4dcf47551276b6a562cd9c
mate <144901301+matekando32243@users.noreply.github.com>
2024-05-13 19:43:25 +0200

    Add files via upload

---
+++ new végleges/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('sidebar-toggle').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('collapsed');
+    document.getElementById('sidebar-content').classList.toggle('collapsed');
+  });
